0630851a73bc45efe1597b2c3478dbebd90a0722
Fernanda Bastos ✸ <hellofernandabastos@gmail.com>
2026-06-15 13:41:02 -0300

    Refactor test_update_product for clarity and updates

--- week_003/tests/test_update_product.py
+++ week_003/tests/test_update_product.py
@@ -1,19 +1,19 @@
-# ======================================== ARQUIVO CRIADO POR FERNANDA BASTOS (@codebyfernanda) ========================================
+# ======================================== FILE CREATED BY FERNANDA BASTOS (@codebyfernanda) ========================================
 
 import requests
 import pytest
 import uuid 
 
-# Atualizar Produto (PUT)
+# Update Product (PUT)
 def test_update_product(my_product_fixture):
     prod_id = my_product_fixture["id"]
     token = my_product_fixture["token"]
     url = f"https://compassuol.serverest.dev/produtos/{prod_id}"
     
     payload = {
-        "nome": f"Logitech MX Vertical ATUALIZADO {uuid.uuid4()}",
+        "nome": f"Logitech MX UPDATED {uuid.uuid4()}",
         "preco": 500,
-        "descricao": "Mouse ergonômico",
+        "descricao": "Mouse",
         "quantidade": 100
     }
     
@@ -25,13 +25,15 @@ def test_update_product(my_product_fixture):
         response = requests.put(url, json=payload, headers=headers)
         response_body = response.json()
         
-        print(f"\nCorpo da resposta da API (PUT em ID existente): {response_body}")
+        print(f"\nAPI response body (PUT on existing ID): {response_body}")
         
         assert response.status_code == 200
         
-        assert "message" in response_body, f"A chave 'message' não veio na resposta! {response_body}"
-        assert response_body["message"] == "Registro alterado com sucesso"
-        print(f"\nStatus retornado para ATUALIZAÇÃO DE PRODUTO COM SUCESSO: {response.status_code}")
+        assert "message" in response_body, f"The 'message' key was not found in the response! {response_body}"
+        assert response_body["message"] == "Record updated successfully"
+        print(f"\nStatus returned for SUCCESSFUL PRODUCT UPDATE: {response.status_code}")
         
     except requests.exceptions.RequestException as error_update_product_sucessfully:
-        pytest.fail(f"A API está retornando um erro na atualização do produto. Erro: {error_update_product_sucessfully}")
+        pytest.fail(f"The API is returning an error upon product update. Error: {error_update_product_sucessfully}")
+
+# ===================================================================================================================================
