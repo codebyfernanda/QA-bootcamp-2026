671391e1722767111beedbc00df6439432a99c9d
Fernanda Bastos ✸ <hellofernandabastos@gmail.com>
2026-06-15 13:35:48 -0300

    Translate test_search_user_by_id to English

--- week_003/tests/test_search_user_by_id.py
+++ week_003/tests/test_search_user_by_id.py
@@ -1,9 +1,9 @@
-# ======================================== ARQUIVO CRIADO POR FERNANDA BASTOS (@codebyfernanda) ========================================
+# ======================================== FILE CREATED BY FERNANDA BASTOS (@codebyfernanda) ========================================
 
 import requests
 import pytest
 
-# Buscar por ID existente
+# Search by existing ID
 def test_search_user_by_id(my_user_fixture):
     user_id = my_user_fixture["id"]
     url = f"https://compassuol.serverest.dev/usuarios/{user_id}"
@@ -12,7 +12,9 @@ def test_search_user_by_id(my_user_fixture):
         response = requests.get(url)
         assert response.status_code == 200
         assert response.json()["_id"] == user_id
-        print(f"\nStatus retornado para BUSCA POR ID EXISTENTE: {response.status_code}")
+        print(f"\nStatus returned for SEARCH BY EXISTING ID: {response.status_code}")
         
     except requests.exceptions.RequestException as error_search_user_existent:
-        pytest.fail(f"A API está retornando um erro na busca por ID existente. Erro: {error_search_user_existent}")
+        pytest.fail(f"The API is returning an error when searching by an existing ID. Error: {error_search_user_existent}")
+
+# ===================================================================================================================================
