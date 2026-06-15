87eea3326e282595b3115c0f1d03101641bf9c33
Fernanda Bastos ✸ <hellofernandabastos@gmail.com>
2026-06-15 13:17:40 -0300

    Update error message for duplicate email test

--- week_003/tests/test_create_user_duplicated_email.py
+++ week_003/tests/test_create_user_duplicated_email.py
@@ -1,17 +1,17 @@
-# ======================================== ARQUIVO CRIADO POR FERNANDA BASTOS (@codebyfernanda) ========================================
+# ======================================== FILE CREATED BY FERNANDA BASTOS (@codebyfernanda) ========================================
 
 import requests 
 
-# Falha ao cadastrar por causa de e-mail duplicado (400)
 ENDPOINT= "https://compassuol.serverest.dev/usuarios" 
 
-
-# O teste em si
+# The test
 def test_create_user_duplicated_email(my_user_fixture):
     payload = my_user_fixture["payload"]
     
     response = requests.post(ENDPOINT, json=payload)
     
     assert response.status_code == 400
-    assert response.json()["message"] == "Este email já está sendo usado"
-    print(f"\nStatus retornado para E-MAIL DUPLICADO: {response.status_code}.")
+    assert response.json()["message"] == "This email is already being used"
+    print(f"\nStatus returned for DUPLICATE E-MAIL: {response.status_code}.")
+
+# ===================================================================================================================================
