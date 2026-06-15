f015b8e4784ef22a2b0b090bea884ff68f173aff
Fernanda Bastos ✸ <hellofernandabastos@gmail.com>
2026-06-15 13:43:39 -0300

    Refactor test for updating non-existent user ID

--- week_003/tests/test_update_user_nonexistent_id.py
+++ week_003/tests/test_update_user_nonexistent_id.py
@@ -1,17 +1,17 @@
-# ======================================== ARQUIVO CRIADO POR FERNANDA BASTOS (@codebyfernanda) ========================================
+# ======================================== FILE CREATED BY FERNANDA BASTOS (@codebyfernanda) ========================================
 
 import requests
 import pytest
 import uuid
 
-# Atualização de ID inexistente (201)
+# Update non-existent ID (201)
 def test_update_user_nonexistent_id():
     user_id = "abc123xyz4567890" 
     url = f"https://compassuol.serverest.dev/usuarios/{user_id}"
     
     payload = {
-        "nome": "Fernanda Bastos Atualizado",
-        "email": f"teste_atualizado_{uuid.uuid4()}@qa.com.br",
+        "nome": "Fernanda Bastos Updated",
+        "email": f"testing{uuid.uuid4()}@qa.com.br",
         "password": "Teste!123",
         "administrador": "true"
     }
@@ -20,13 +20,15 @@ def test_update_user_nonexistent_id():
         response = requests.put(url, json=payload)
         response_body = response.json()
         
-        print(f"\nCorpo da resposta da API (PUT em ID inexistente): {response_body}")
+        print(f"\nAPI response body (PUT on non-existent ID): {response_body}")
         
-        # Como o ID não existe, o ServeRest cria um novo usuário
+        # Since the ID does not exist, ServeRest creates a new user
         assert response.status_code == 201
         
-        assert "message" in response_body, f"A chave 'message' não veio na resposta! {response_body}"
-        assert response_body["message"] == "Cadastro realizado com sucesso"
+        assert "message" in response_body, f"The 'message' key was not found in the response! {response_body}"
+        assert response_body["message"] == "Registration completed successfully"
         
     except requests.exceptions.RequestException as error_update_user_nonexistent_id:
-        pytest.fail(f"A requisição falhou devido a um erro de conexão: {error_update_user_nonexistent_id}")
+        pytest.fail(f"The request failed due to a connection error: {error_update_user_nonexistent_id}")
+
+# ===================================================================================================================================
