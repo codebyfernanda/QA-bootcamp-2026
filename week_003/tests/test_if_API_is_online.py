42344bd172ed841be25d41cf580e192578be514a
Fernanda Bastos ✸ <hellofernandabastos@gmail.com>
2026-06-15 13:25:58 -0300

    Update comments and print statements in API test

--- week_003/tests/test_if_API_is_online.py
+++ week_003/tests/test_if_API_is_online.py
@@ -1,18 +1,20 @@
-# ======================================== ARQUIVO CRIADO POR FERNANDA BASTOS (@codebyfernanda) ========================================
+# ======================================== FILE CREATED BY FERNANDA BASTOS (@codebyfernanda) ========================================
 
 import requests
 import pytest
 
 ENDPOINT = "https://compassuol.serverest.dev/" 
 
-# Testar se a API está online
+# Testing if API is online
 def test_if_API_is_online():
     url = "https://compassuol.serverest.dev/"
     
     try:
         response = requests.get(url)
         assert response.status_code == 200
-        print(f"\nStatus retornado para API ONLINE: {response.status_code}")
+        print(f"\nStatus returned for ONLINE API: {response.status_code}")
         
     except requests.exceptions.RequestException as error_not_online:
-        pytest.fail(f"A API está offline ou inacessível. Erro: {error_not_online}")
+        pytest.fail(f"The API is offline or unreachable. Error: {error_not_online}")
+
+# ===================================================================================================================================
